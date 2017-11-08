82d69691c57c04cb15e929caad35569118d35fcf
josephjithin <josephgithin@gmail.com>
2017-11-08 15:43:57 +0530

    voice integration

--- src/main/java/com/iot/net/AIController.java
+++ src/main/java/com/iot/net/AIController.java
@@ -2,19 +2,17 @@ package com.iot.net;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("ai")
 public class AIController {
 
     @RequestMapping(path="command",method = {RequestMethod.POST,RequestMethod.GET})
-    public String command(@RequestBody String object) {
+    public @ResponseBody
+    WebHookResponse command(@RequestBody String object) {
         System.out.println(object);
-        return new String("OK");
+        return new WebHookResponse("Operation Successfull","Operation Succesfull");
 
     }
 
---
+++ src/main/java/com/iot/net/WebHookResponse.java
@@ -0,0 +1,25 @@
+package com.iot.net;
+
+public class WebHookResponse {
+    private final String speech;
+    private final String displayText;
+
+    private final String source = "java-webhook";
+
+    public WebHookResponse(String speech, String displayText) {
+        this.speech = speech;
+        this.displayText = displayText;
+    }
+
+    public String getSpeech() {
+        return speech;
+    }
+
+    public String getDisplayText() {
+        return displayText;
+    }
+
+    public String getSource() {
+        return source;
+    }
+}
