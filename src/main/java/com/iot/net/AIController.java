a626b70fd5d67bfe4c36cd10d16744c1b80a5075
josephjithin <josephgithin@gmail.com>
2017-11-08 13:59:46 +0530

    voice integration

--- src/main/java/com/iot/net/AIController.java
+++ src/main/java/com/iot/net/AIController.java
@@ -1,18 +1,15 @@
 package com.iot.net;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 @RequestMapping("/ai")
 public class AIController {
 
     @RequestMapping(method= RequestMethod.POST)
-    public @ResponseBody
-    void sayHello(String values) {
+    public void sayHello(@RequestBody String values) {
         System.out.println(values);
     }
 
