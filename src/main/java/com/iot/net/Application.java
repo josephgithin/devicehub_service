47dae84ed0302bbb766e946aac8962093bd7c3b1
josephjithin <josephgithin@gmail.com>
2017-11-09 21:45:06 +0530

    added speech synthesizer

--- src/main/java/com/iot/net/Application.java
+++ src/main/java/com/iot/net/Application.java
@@ -11,10 +11,12 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
 
         Constants.objectPinMapping.put("lights","12");
-        Constants.objectPinMapping.put("light","13");
+        Constants.objectPinMapping.put("light","12");
+        Constants.objectPinMapping.put("television","13");
         Constants.objectPinMapping.put("fan","14");
-        Constants.objectPinMapping.put("fans","15");
+        Constants.objectPinMapping.put("music","15");
         Constants.roomDeviceMapping.put("living","DEV2");
+        Constants.roomDeviceMapping.put("bed","DEV1");
 
         SpringApplication.run(Application.class, args);
 
